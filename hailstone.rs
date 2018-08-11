a0e6e02bf8b2ed36fc9c41c02f19568059f44d83
Kathleen Doviken <kathleendoviken@gmail.com>
2018-08-11 11:17:35 -0500

    hailstone
    simple hailstone implementation in rust

---
+++ hailstone.rs
@@ -0,0 +1,33 @@
+use std::io;
+use std::io::Write;
+
+fn main() {
+    
+    print!("Enter number: ");
+    io::stdout().flush().unwrap();
+
+    let mut num = String::new();
+
+    io::stdin()
+        .read_line(&mut num)
+        .expect("Failed to read line");
+
+    let mut num = num.trim().parse::<u32>().expect("NaN");
+
+    let mut num_steps = 0;
+
+    while num > 1 {
+        if num%2 == 0 {
+            let new_num = num/2;
+            print!("{} ", new_num);
+            num = new_num;
+        } else if num%2 == 1 {
+            let new_num = (num*3) + 1;
+            print!("{} ", new_num);
+            num = new_num;
+        }
+        num_steps+=1;
+    }
+    println!();
+    println!("This process took {} steps to reach 1", num_steps);
+}
